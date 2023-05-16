2f6c5d5f162a93acc614bd0033aa68fb12a71410
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2023-05-16 15:59:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-def acceptare(cuvant,s,gram):
+def acceptare(cuvant, s, gram):
     if not cuvant:
         return "lambda" in gram[s]
     for x in gram[s]:
-        if x[0]==cuvant[0] :
-            if acceptare(cuvant[1:],x[1:],gram):
+        if x[0] == cuvant[0]:
+            if acceptare(cuvant[1:], x[1:], gram):
                 return True
     return False
 
-f=open("gramatica","r")
-gram=dict()
-for linie in f:
-    linie=linie.split("->")
-    simbol=linie[0].strip()
-    regula=linie[1].strip()
-    regula=regula.split("|")
-    regula=[x.strip() for x in regula]
-    gram.update({simbol:regula})
-f.close()
-f=open("cuvinte")
-cuvinte=[linie.strip() for linie in f]
-f.close()
-f=open("simbol")
-s=f.read()
-f.close()
+
+with open("gramatica", "r") as f:
+    gram = {}
+    for linie in f:
+        linie = linie.split("->")
+        simbol = linie[0].strip()
+        regula = linie[1].strip()
+        regula = regula.split("|")
+        regula = [x.strip() for x in regula]
+        gram[simbol] = regula
+
+with open("cuvinte", "r") as f:
+    cuvinte = [linie.strip() for linie in f]
+
+with open("simbol", "r") as f:
+    s = f.read()
+
 for cuv in cuvinte:
-    if acceptare(cuv,s,gram):
+    if acceptare(cuv, s, gram):
         print(f"Cuvantul {cuv} este acceptat de gramatica")
     else:
         print(f"Cuvantul {cuv} nu este acceptat de gramatica")
-
